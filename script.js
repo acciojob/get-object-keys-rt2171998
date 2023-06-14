fa58b01874f10317f04ad7906f7aa0547ae11deb
rt2171998 <70574052+rt2171998@users.noreply.github.com>
2023-06-14 17:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let student = {
+  name: "Felicia"
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+console.log(student.getKeys());
